d3993839c54a69ab211bf41800ce2accb9afb1e8
Hakob2046 <melkonyan7788@gmail.com>
2025-08-16 12:20:14 +0400

    miban

--- RestProject/RestProject/settings.py
+++ RestProject/RestProject/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,12 +83,16 @@ WSGI_APPLICATION = "RestProject.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default':  { 
+        'ENGINE': 
+        'django.db.backends.postgresql', 
+        'USER': 'postgres', 
+        'NAME': os.getenv('PSG_NAME'),
+        'PASSWORD': os.getenv('PSG_PASSWORD'), 
+        'HOST': 'localhost', 
+        'PORT': '5432', 
     }
-}
-
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
